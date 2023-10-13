9095176fdd06dbc269953853b3e5d136ca64811d
baydev <baydev2002@gmail.com>
2023-10-13 19:35:54 +0300

    Added a structure

---
+++ src/main/java/bay/university/resmon/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package bay.university.resmon.controller;
+
+import bay.university.resmon.model.Employee;
+import bay.university.resmon.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+}
