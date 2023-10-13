93b6202481cb007f159dab45e2ed51bf88bd5b89
baydev <baydev2002@gmail.com>
2023-10-13 19:34:41 +0300

    Start of a big project

---
+++ src/main/java/bay/university/resmon/ResourceMonitorApplication.java
@@ -0,0 +1,12 @@
+package bay.university.resmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bay/university/resmon/ResourceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package bay.university.resmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
